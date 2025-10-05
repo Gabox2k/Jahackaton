523755263197f0e44452b180eb7d7ca98a21c908
Gabriel Raidan <sforza1383@gmail,com>
2025-10-05 01:57:14 -0300

    se agrego una segunda imagen

--- menu.py
+++ menu.py
@@ -3,27 +3,31 @@ import sys
 
 pygame.init()
 
-# Configuración de ventana
+#configuracion de la ventana
 ancho, alto = 1152, 758
 ventana = pygame.display.set_mode((ancho, alto))
 pygame.display.set_caption("Tembi'u Rush - Menú Principal")
 
-# Fondo
+#fondo
 fondo = pygame.image.load("menu.png").convert()
 fondo = pygame.transform.scale(fondo, (ancho, alto))
 
-# Colores estilo pixel-art
+fondo_inst = pygame.image.load("fondo_instrucciones.jpg").convert()
+fondo_inst = pygame.transform.scale(fondo_inst,(ancho, alto))
+
+
+#Colores de estilo
 crema = (252, 242, 210)
 verde_borde = (64, 128, 64)
 verde_texto = (48, 96, 48)
 
-# Fuente pixel art
+#Las fuentes de pixel-art
 try:
     fuente_pixel = pygame.font.Font("PressStart2P-Regular.ttf", 40)
 except:
     fuente_pixel = pygame.font.Font(None, 60)
 
-# Rectángulos de botones (uno al lado del otro)
+#Rectangulos de los botones
 boton_juego = pygame.Rect(ancho // 2 - 400, 600, 300, 80)
 boton_salir = pygame.Rect(ancho // 2 + 100, 600, 300, 80)
 
@@ -39,6 +43,8 @@ def dibujar_boton(superficie, rect, texto, fuente, color_texto, color_fondo, col
          rect.centery - render_texto.get_height() // 2)
     )
 
+pantalla_juego = False
+
 while True:
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
@@ -46,17 +52,21 @@ while True:
             sys.exit()
         
         if evento.type == pygame.MOUSEBUTTONDOWN:
-            if boton_juego.collidepoint(evento.pos):
-                print("Iniciando el juego")
-            if boton_salir.collidepoint(evento.pos):
-                pygame.quit()
-                sys.exit()
-    
-    ventana.blit(fondo, (0, 0))
-
-    # Dibujar botones estilo pixel-art
-    dibujar_boton(ventana, boton_juego, "Jugar", fuente_pixel, verde_texto, crema, verde_borde)
-    dibujar_boton(ventana, boton_salir, "Salir", fuente_pixel, verde_texto, crema, verde_borde)
+            if not pantalla_juego:
+                if boton_juego.collidepoint(evento.pos):
+                    pantalla_juego = True
+                    print("Iniciando el juego")
+                if boton_salir.collidepoint(evento.pos):
+                    pygame.quit()
+                    sys.exit()
+        
+    if pantalla_juego:
+        ventana.blit(fondo_inst, (0, 0))
+    else:
+        ventana.blit(fondo, (0, 0))   
+        #Dibujar botones estilo pixel-art
+        dibujar_boton(ventana, boton_juego, "Jugar", fuente_pixel, verde_texto, crema, verde_borde)
+        dibujar_boton(ventana, boton_salir, "Salir", fuente_pixel, verde_texto, crema, verde_borde)
 
     pygame.display.flip()
     clock.tick(30)
