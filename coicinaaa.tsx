788f1ea7d1e02f5dd2d6f55f5155957ae1d7bf3c
Eduardo Bozzano <bozzanoedu@gmail.com>
2025-10-05 09:16:57 -0300

    Create coicinaaa.tsx

---
+++ coicinaaa.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.1-99-gec89c545" name="coicinaaa" tilewidth="32" tileheight="32" tilecount="1008" columns="42">
+ <image source="../Jahackaton/image_b30b5d.jpg" width="1344" height="768"/>
+ <tile id="970">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
